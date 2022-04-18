3c2cccc2e3cf07b6cf8fd1294bf148ac9edad60c
shobhitchoudhary745 <shobhitchoudhary745@gmail.com>
2022-04-18 17:38:17 +0530

    finalcommit

--- src/emails/email.js
+++ src/emails/email.js
@@ -7,7 +7,7 @@ const sendWelcomeEmail=(email,name)=>{
         to:email,
         from:'coding.souls@sdbc.ac.in',
         subject:'For joining coding soul',
-        html:`<p>You are Registered Successfully. please attach your college I'd and payment receipt to:<br/>Email:anmolagrawal873@gmail.com<br/><br/>For any other query contact us at: 9131399266</p><h3>Thanks and regards!</h3><p>Team coding souls.`
+        html:`<p>You are Registered Successfully. please attach your college I'd and payment receipt to:<br/>Email:coding.souls@sdbc.ac.in<br/><br/>For any other query contact us at: 9131399266</p><h3>Thanks and regards!</h3><p>Team Coding Souls.`
      }) 
 }
 
